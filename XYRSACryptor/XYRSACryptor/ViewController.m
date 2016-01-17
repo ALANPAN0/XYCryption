63119ef73915261974c79641ecc78c87ee2acf7f
Panda <panworkemail@163.com>
2016-01-17 15:14:50 +0800

    adjust file architecture

--- XYRSACryptor/XYRSACryptor/ViewController.m
+++ XYRSACryptor/XYRSACryptor/ViewController.m
@@ -34,7 +34,7 @@
     NSString *p12Path = [[NSBundle mainBundle] pathForResource:@"private_key" ofType:@"p12"];
     [self.rsaCryptor loadPrivateKeyFromFile:p12Path password:@"123456"];
 
-    NSString *enStr = @"Panda_iOS 喜欢的请关注我 Panda_iOS 喜欢的请关注我Panda_iOS 喜欢的请关注我Panda_iOS 喜欢的请关注我 Panda_iOS 喜欢的请关注我Panda_iOS 喜欢的请关注我Panda_iOS 喜欢的请关注我Panda_iOS ";
+    NSString *enStr = @"请替换为你要加密的文本内容！";
 
     //加解密
     NSData *enData = [self.rsaCryptor rsaEncryptData:
@@ -43,14 +43,14 @@
     
     
     NSString *deStr = [[NSString alloc] initWithData:deData encoding:NSUTF8StringEncoding];
-    NSLog(@"加密前： %@ \n 解密后： %@",enStr,deStr);
+    NSLog(@"\n 加密前： %@ \n 解密后： %@",enStr,deStr);
     
     //签名
     NSData *signedData = [self.sign sha256WithRSA:enData privateKey:[self.sign getPrivateKeyRefWithP12Path:p12Path]];
-    NSLog(@"签名后的base64:%@",[GTMBase64 stringByEncodingData:signedData]);
+    NSLog(@"\n 签名后的base64:%@",[GTMBase64 stringByEncodingData:signedData]);
     
     BOOL signSuccess = [self.rsaCryptor rsaSHA256VerifyData:enData withSignature:signedData];
-    NSLog(@"是否签名成功：%@",signSuccess ? @"是":@"否");
+    NSLog(@"\n 是否签名成功：%@",signSuccess ? @"是":@"否");
 }
 
 - (XYRSACryptor *)rsaCryptor {
